104e807401d31599dc0768bc9a2b18469c5e6718
Albert Dias <lordfake13dk@gmail.com>
2025-08-22 02:48:30 -0300

    created endpoint ./games/{id} 'get games by id'

---
+++ src/main/java/com/laerze/projetoGames/dto/GameDTO.java
@@ -0,0 +1,66 @@
+package com.laerze.projetoGames.dto;
+
+import com.laerze.projetoGames.entities.Game;
+
+public class GameDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDTO() {}
+
+    public GameDTO(Game game) {
+        this.id = game.getId();
+        this.title = game.getTitle();
+        this.year = game.getYear();
+        this.genre = game.getGenre();
+        this.platforms = game.getPlatforms();
+        this.score = game.getScore();
+        this.imgUrl = game.getImgUrl();
+        this.shortDescription = game.getShortDescription();
+        this.longDescription = game.getLongDescription();
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
--- src/main/java/com/laerze/projetoGames/services/GameService.java
+++ src/main/java/com/laerze/projetoGames/services/GameService.java
@@ -1,9 +1,13 @@
 package com.laerze.projetoGames.services;
 
+import com.laerze.projetoGames.dto.GameDTO;
 import com.laerze.projetoGames.dto.GameMinDTO;
+import com.laerze.projetoGames.entities.Game;
 import com.laerze.projetoGames.repositories.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,9 +18,17 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream()
                 .map(GameMinDTO::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game game = gameRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Jogo de id " + id + "não encontrado."));
+        return new GameDTO(game);
     }
 }
